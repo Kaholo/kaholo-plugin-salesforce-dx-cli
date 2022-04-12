fa458480e0c0cf8441343c6278ca6c3300ec4a91
Kacper Debowski <92038478+kdebowski-kaholo@users.noreply.github.com>
2022-04-12 10:25:13 +0200

    [KP-698] - [Salesforce DX CLI] Use Docker to call CLI (#3)
    * Remade the plugin to use docker for all command executions + added `run command` method
    
    * Fixed error handling
    
    * Removed error handler and redundant env var

--- app.js
+++ app.js
@@ -1,66 +1,53 @@
+const kaholo = require("kaholo-plugin-library");
 const SFDX = require("./sfdx");
-const {
-  mergeInputs,
-  errorHandler,
-} = require("./utils");
+const utils = require("./utils");
 
-async function deployProject(action, settings) {
+async function deployProject(parameters) {
   const {
-    consumerKey,
-    jwtKey,
-    username,
     sourceDirectory,
-    instanceUrl,
     testLevel,
     outputJson,
-  } = mergeInputs(action.params, settings);
+  } = parameters;
 
-  await SFDX.authenticate({
-    consumerKey,
-    jwtKey,
-    username,
-    instanceUrl,
-    sourceDirectory,
-    outputJson,
-  });
+  const credentials = utils.getCredentials(parameters);
 
   return SFDX.deployProject({
+    credentials,
     sourceDirectory,
     testLevel,
-    username,
     outputJson,
   });
 }
 
-async function validateProject(action, settings) {
+async function validateProject(parameters) {
   const {
-    username,
-    consumerKey,
-    jwtKey,
     sourceDirectory,
-    instanceUrl,
     testLevel,
     outputJson,
-  } = mergeInputs(action.params, settings);
+  } = parameters;
 
-  await SFDX.authenticate({
-    consumerKey,
-    jwtKey,
-    username,
-    instanceUrl,
-    sourceDirectory,
-    outputJson,
-  });
+  const credentials = utils.getCredentials(parameters);
 
   return SFDX.validateProject({
+    credentials,
     sourceDirectory,
     testLevel,
-    username,
     outputJson,
   });
 }
 
-module.exports = {
-  deployProject: errorHandler(deployProject),
-  validateProject: errorHandler(validateProject),
-};
+async function runCommand(parameters) {
+  const credentials = utils.getCredentials(parameters);
+
+  return SFDX.runCommand({
+    credentials,
+    command: parameters.command,
+    outputJson: parameters.outputJson,
+  });
+}
+
+module.exports = kaholo.bootstrap({
+  runCommand,
+  deployProject,
+  validateProject,
+}, {});
--- sfdx.js
+++ sfdx.js
@@ -1,92 +1,79 @@
 const util = require("util");
-const exec = util.promisify(require("child_process").exec);
+const _ = require("lodash");
+const childProcess = require("child_process");
 
-const {
-  createJWTFile,
-  deleteJWTFile,
-  JWT_KEY_FILE_DIRECTORY,
-} = require("./jwtFile");
+const exec = util.promisify(childProcess.exec);
 
-const CLI = "sfdx";
+// eslint-disable-next-line no-multi-str
+const SFDX_AUTH_COMMAND = "\
+export KEY_FILE=$(mktemp) && \
+echo \"$SFDX_KEY\" > $KEY_FILE && \
+sfdx auth:jwt:grant \
+--username $SFDX_USERNAME \
+--clientid $SFDX_CLIENT_ID \
+--jwtkeyfile $KEY_FILE 1>/dev/null && \
+rm $KEY_FILE && \
+unset KEY_FILE";
+
+// eslint-disable-next-line no-multi-str
+const DOCKER_SFDX_CLI_COMMAND = "\
+docker run \
+-e SFDX_KEY \
+-e SFDX_USERNAME \
+-e SFDX_CLIENT_ID \
+-e SFDX_AUTH_COMMAND \
+-e SANITIZED_COMMAND \
+-e PROJECT_PATH \
+-v $PROJECT_PATH:/project \
+-w /project \
+--rm salesforce/salesforcedx:latest-full  \
+bash -c \"$SFDX_AUTH_COMMAND && sfdx $SANITIZED_COMMAND \"";
+
 const COMMANDS = {
-  AUTHENTICATE: "auth:jwt:grant",
   DEPLOY: "force:source:deploy",
 };
 
-async function authenticate({
-  consumerKey, jwtKey, username, instanceUrl, sourceDirectory, outputJson,
-}) {
-  try {
-    await createJWTFile({
-      sourceDirectory,
-      jwtKey,
-    });
-    await callAuthenticateCommand({
-      username,
-      consumerKey,
-      instanceUrl,
-      sourceDirectory,
-      outputJson,
-    });
-  } finally {
-    await deleteJWTFile({
-      sourceDirectory,
-    });
+function sanitizeCommand(command) {
+  let sanitized = command;
+  if (_.startsWith(command.toLowerCase(), "sfdx ")) {
+    sanitized = command.slice(5);
   }
+
+  // This is the safest way to escape the user provided command.
+  // By putting the command in double quotes, we can be sure that
+  // every character within the command is escaped, including the
+  // ones that could be used for shell injection (e.g. ';', '|', etc.).
+  // The escaped string needs then to be echoed back to the docker command
+  // in order to be properly executed - simply passing the command in double quotes
+  // would result in docker confusing the quotes as a part of the command.
+  return `$(echo "${sanitized}")`;
 }
 
 function validateProject({
+  credentials,
   sourceDirectory,
   testLevel,
-  username,
   outputJson,
 }) {
   return callDeployCommand({
+    credentials,
     sourceDirectory,
     testLevel,
-    username,
     outputJson,
     extraParamsString: "--checkonly",
   });
 }
 
-async function callAuthenticateCommand({
-  username, consumerKey, instanceUrl, sourceDirectory, outputJson,
-}) {
-  let params = `\
---clientid ${consumerKey} \
---jwtkeyfile ${JWT_KEY_FILE_DIRECTORY} \
---username ${username}`;
-
-  if (outputJson) {
-    params += " --json";
-  }
-  if (instanceUrl) {
-    params += ` --instanceUrl ${instanceUrl}`;
-  }
-
-  try {
-    const output = await exec(
-      `${CLI} ${COMMANDS.AUTHENTICATE} ${params}`,
-      { cwd: sourceDirectory },
-    );
-
-    return output.stdout;
-  } catch (error) {
-    throw new SFDXError(`authentication failed: ${error}`);
-  }
-}
-
 async function callDeployCommand({
+  credentials,
   sourceDirectory,
   testLevel,
-  username,
   outputJson,
   extraParamsString,
 }) {
   let params = `\
--p ${sourceDirectory} \
--u ${username}`;
+-p /project \
+-u ${credentials.SFDX_USERNAME}`;
 
   if (outputJson) {
     params += " --json";
@@ -98,17 +85,59 @@ async function callDeployCommand({
     params += ` ${extraParamsString}`;
   }
 
-  try {
-    const output = await exec(
-      `${CLI} ${COMMANDS.DEPLOY} ${params}`,
-      { cwd: sourceDirectory },
-    );
+  return execute(credentials, `${COMMANDS.DEPLOY} ${params}`, sourceDirectory);
+}
 
-    return output.stdout;
-  } catch (error) {
-    const { stdout } = error;
-    throw new SFDXError(`deployment failed: ${error}. ${stdout}`);
+async function runCommand({
+  credentials,
+  command,
+  outputJson,
+}) {
+  let params = "";
+  if (outputJson) {
+    params += " --json";
   }
+
+  return execute(credentials, `${command} ${params}`);
+}
+
+function formatOutput(output) {
+  let { stderr, stdout } = output;
+
+  try {
+    stdout = JSON.parse(stdout);
+    stderr = JSON.parse(stderr);
+  } catch (error) {
+    // do nothing
+  }
+
+  if (stderr && !stdout) {
+    throw new SFDXError(`Command failed: ${stderr}`);
+  }
+
+  return {
+    stderr,
+    stdout,
+  };
+}
+
+async function execute(credentials, command, projectPath) {
+  let output;
+  try {
+    output = await exec(DOCKER_SFDX_CLI_COMMAND, {
+      env: {
+        ...credentials,
+        SFDX_AUTH_COMMAND,
+        RAW_COMMAND: command,
+        SANITIZED_COMMAND: sanitizeCommand(command),
+        PROJECT_PATH: projectPath,
+      },
+    });
+  } catch (error) {
+    throw new SFDXError(`Command failed: ${error.stderr}`);
+  }
+
+  return formatOutput(output);
 }
 
 class SFDXError extends Error {
@@ -118,7 +147,7 @@ class SFDXError extends Error {
 }
 
 module.exports = {
-  authenticate,
   deployProject: callDeployCommand,
   validateProject,
+  runCommand,
 };
